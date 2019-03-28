a93a2fd79f993cbdaa4d0251f4425e1954fb3b1a
Kyle McGuffin <kyle.mcguffin@vumc.org>
2019-03-28 08:29:28 -0400

    Add patient search ajax

--- patientLookup.php
+++ patientLookup.php
@@ -6,8 +6,25 @@ if($project == "") {
 	throw new Exception("No project selected");
 }
 
+
+/* @var $module RedcapAfrica\PatientLookupModule\PatientLookupModule */
 require_once \ExternalModules\ExternalModules::getProjectHeaderPath();
+?>
+<span>Search for Patient</span> <input id='patient_search' size='40' type='text' onchange='lookupPatient($(this).val());' /><Br />
 
+<div id='patient_results'>
+</div>
 
+<script type='text/javascript'>
+	function lookupPatient(string) {
+		$.ajax({
+			method:"POST",
+			url: "<?php echo $module->getUrl("patientSearchAjax.php"); ?>"
+		}).done(function(html) {
+			$('#patient_results').html(html);
+		});
+	}
+</script>
 
+<?php
 require_once \ExternalModules\ExternalModules::getProjectFooterPath();
