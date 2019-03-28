68e78edb509b90d9ef896d4b227a3a7dbadab119
Kyle McGuffin <kyle.mcguffin@vumc.org>
2019-03-28 09:30:02 -0400

    Make the ajax go to the buttons

--- patientSearchAjax.php
+++ patientSearchAjax.php
@@ -7,6 +7,7 @@ if($project == "") {
 
 /* @var $module RedcapAfrica\PatientLookupModule\PatientLookupModule */
 $lookupFields = $module->getProjectSetting("matching-fields");
+$displayFields = $module->getProjectSetting("display-fields");
 
 $searchValue = strtolower($_POST['searchValue']);
 
@@ -48,4 +49,33 @@ while($row = db_fetch_assoc($q)) {
 	$recordIds[] = $row["record"];
 }
 
-var_dump($recordIds);
+foreach($recordIds as $recordId) {
+	$displayString = "";
+	$recordDetails = $module->getData($project,$recordId);
+
+	$displayData = [];
+	foreach($recordDetails as $recordId => $eventDetails) {
+		foreach($eventDetails as $eventId => $details) {
+			foreach($displayFields as $thisField) {
+				if($details[$thisField] != "") {
+					$displayData[$thisField] = $details[$thisField];
+				}
+			}
+		}
+	}
+
+	$metadata = $module->getMetadata($project);
+
+	foreach($displayFields as $thisField) {
+		$displayString .= $metadata[$thisField]["field_label"]." : ".$displayData[$thisField]."<br />";
+	}
+
+	## Add button to edit record to results
+	$displayString .= "<button onclick='window.location.href=app_path_webroot_full+app_path_webroot+\"DataEntry/record_home.php?pid=\"+pid+\"&id=".$recordId."'>Go to Record</button><Br />";
+
+	echo "<div style='border:solid black 1px; width:200px' >$displayString</div>";
+}
+
+if(count($recordIds) == 0 ) {
+	echo "<button onclick='alert(app_path_webroot_full+app_path_webroot+page+\"?pid=\"+pid+\"&id=672&auto=1&arm=\"+($(\"#arm_name_newid\").length))' >Add New Record</button>";
+}
